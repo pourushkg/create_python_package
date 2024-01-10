fcaa124a20a0f3fa3a54439b285e367d3e670345
Pourush Kumar Gupta <pkg.21p10161@mtech.nitdgp.ac.in>
2024-01-10 10:48:11 +0530

    everything_updated

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirement(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+   
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+
+__version__ = "0.0.4"
+REPO_NAME = "create_python_package"
+PKG_NAME= "MongoDB-Connect"
+AUTHOR_USER_NAME = "pourushkg"
+AUTHOR_EMAIL = "pkg.21p10161@mtech.nitdgp.ac.in"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires = get_requirement("./requirements_dev.txt")
+    
+    
+)
